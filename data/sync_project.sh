4633282064fc56ae3c764a7a32ed00f8dc1382d1
illia <illya.tanasyuk@gmail.com>
2020-01-20 18:42:24 +0200

    added project synchronization file

---
+++ data/sync_project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+scp -r /home/illia/myFiles/projects/five_star/scripts/ illia@vps721220.ovh.net:/home/illia/code/five_star/
+scp -r /home/illia/myFiles/projects/five_star/data/ illia@vps721220.ovh.net:/home/illia/code/five_star/
+
+echo transfer ended
+
+
+
